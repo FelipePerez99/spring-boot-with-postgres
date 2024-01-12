89ca23b95e83e0632e7965a8ca8defef9f2a001b
Felipe <andresfppy@gmail.com>
2024-01-12 14:48:09 -0500

    first commit

---
+++ src/test/java/com/springpg/SpringpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
